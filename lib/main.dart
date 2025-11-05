49c8c791221ccf0577ab0d4bfc6eb872c77d88d2
mubasheransari <mubashera38@gmail.com>
2025-11-05 16:57:01 +0500

    updations

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Taskoon App',
+      title: 'AMS-T',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
